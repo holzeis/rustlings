6112d92c1cf2dfac930cc03beca944a412fc653d
Richard Holzeis <richard@holzeis.me>
2022-08-01 08:28:07 +0200

    docs: finished intro, ifs, variables, functions, quiz1 and quiz2, primitive types and vecs

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -8,10 +8,17 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples: i32) -> i32 {
+    if apples < 0 {
+        return 0;
+    }
+    if apples <= 40 {
+        return apples * 2;
+    }
+
+    return apples * 1;
+}
 
 // Don't modify this function!
 #[test]
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,8 +18,6 @@
 // - The output element is going to be a Vector of strings.
 // Execute `rustlings hint quiz2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -30,10 +28,24 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(String::from(string.trim())),
+                Command::Append(count) => {
+                    let mut bar = String::from(string);
+                    let mut i = 0;
+                    while i < *count {
+                        bar = bar + "bar";
+                        i = i + 1;
+                    }
+                    output.push(String::from(bar.to_string()));
+                }
+                _ => todo!()
+            }
             // TODO: Complete the function body. You can do it!
         }
         output
@@ -43,7 +55,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What to we have to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]

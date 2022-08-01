aab0ee4e18d364fc033e605ad74113b286526636
Richard Holzeis <richard@holzeis.me>
2022-08-01 16:18:13 +0200

    finished enums, errors, generics, hashmaps, modules, quiz3, strings and traits

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -1,15 +1,12 @@
 // options2.rs
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     let optional_word = Some(String::from("rustlings"));
     // TODO: Make this an if let statement whose value is "Some" type
-    word = optional_word {
-        println!("The word is: {}", word);
-    } else {
-        println!("The optional word doesn't contain anything");
+    match optional_word {
+        Some(word) => println!("The word is: {}", word),
+        None => println!("The optional word doesn't contain anything"),
     }
 
     let mut optional_integers_vec: Vec<Option<i8>> = Vec::new();
@@ -19,7 +16,10 @@ fn main() {
 
     // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
     // You can stack `Option<T>`'s into while let and if let
-    integer = optional_integers_vec.pop() {
-        println!("current value: {}", integer);
+    for option in optional_integers_vec.iter() {
+        match option {
+            Some(integer) => println!("current value: {}", integer),
+            None => (),
+        }
     }
 }

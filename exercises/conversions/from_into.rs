b524ed4ef3c3952ea0bb83b30d4f2667f128e27f
Richard Holzeis <richard@holzeis.me>
2022-08-07 12:00:22 +0200

    completed rustling erercises

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,38 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+
+        let mut split = s.splitn(2, ",");
+
+        let name = match split.next() {
+            Some(name) => {
+                if name.len() == 0 {
+                    return Person::default();
+                }
+                name
+            },
+            None => return Person::default()
+        };
+
+        let age = match split.next() {
+            Some(age) => {
+                match age.parse::<usize>() {
+                    Ok(age) => age,
+                    Err(_) => {
+                        println!("Could not parse {} as age", age);
+                        return Person::default();
+                    }
+                }
+            },
+            None=> return Person::default()
+        };
+
+        Person { name: name.to_string(), age }
     }
 }
 
